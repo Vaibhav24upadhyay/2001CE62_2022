62b96f12ea1a8360ba9b94bf1eab4aae84e6351d
Vaibhav  upadhyay <Vaibhav4upadhyay@gmail.com>
2022-09-14 23:35:34 +0530

    second commit

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,14 +1,33 @@
-def octact_identification(mod=5000):
-###Code
-
-
-from platform import python_version
-ver = python_version()
-
-if ver == "3.8.10":
-    print("Correct Version Installed")
-else:
-    print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
-
 mod=5000
-octact_identification(mod)
+from asyncio.windows_events import NULL
+from itertools import count
+from logging import NullHandler
+from queue import Empty
+from tkinter import W
+import pandas as pd
+import math
+pd.options.mode.chained_assignment = None  # default='warn'
+idf = pd.read_csv('octant_input.csv')
+
+
+# Calculating mean of U , V , W
+U_Avg = idf["U"].mean()
+V_Avg= idf["V"].mean()
+W_Avg = idf["W"].mean()
+
+# inserting mean values
+idf.loc[0,"U Avg"] = U_Avg
+idf.loc[0,"V Avg"] = V_Avg
+idf.loc[0,"W Avg"] = W_Avg
+
+# Calculating U' , V' , W' ,
+idf["U'=U-U_avg"] =""
+idf["V'=V-V_avg"] =""
+idf["W'=W-W_avg"] =""
+idf["U'=U-U_avg"] = idf["U"]-U_Avg
+idf["V'=V-V_avg"] = idf["V"]-V_Avg
+idf["W'=W-W_avg"] = idf["W"]-W_Avg
+
+
+# finding total number of interval {0-5000 ,5001-10000 , -->> so on}
+n=int((idf['U'].size+mod)/mod)
